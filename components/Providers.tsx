038728d69601feaf400e8f23e714db7c0de2d36e
lolla-sai <saisameer.lolla@gmail.com>
2023-08-02 10:32:25 +0530

    Added React Query and Axios

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,11 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import { Inter } from "next/font/google";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
 // 1. Import the extendTheme function
 import { extendTheme } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
+const queryClient = new QueryClient();
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
@@ -33,7 +35,11 @@ function Providers({ children }: { children: React.ReactNode }) {
           }
         `}
       </style>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </ChakraProvider>
     </>
   );
 }
