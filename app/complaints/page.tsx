ae568c8487f3a4e772407cdcae5080ad045be6dd
Kaiser Souza <93536968+KaiserSouza69420@users.noreply.github.com>
2023-08-07 12:23:24 +0530

    updated customer complaints page

--- app/complaints/page.tsx
+++ app/complaints/page.tsx
@@ -1,4 +1,5 @@
-"use client"; 
+"use client"
+
 
 import { useState } from 'react';
 import {
@@ -18,24 +19,31 @@ interface Complaint {
   id: number;
   text: string;
   documents: string[];
+  billId: string;
 }
 
 export default function ComplaintsPage() {
   const [complaintText, setComplaintText] = useState('');
+  const [billId, setBillId] = useState('');
   const [attachedDocuments, setAttachedDocuments] = useState<string[]>([]);
   const [complaints, setComplaints] = useState<Complaint[]>([]);
 
   const handleComplaintSubmit = () => {
-    if (complaintText.trim() !== '') {
-      const newComplaint: Complaint = {
-        id: complaints.length + 1,
-        text: complaintText,
-        documents: attachedDocuments,
-      };
-      setComplaints([...complaints, newComplaint]);
-      setComplaintText('');
-      setAttachedDocuments([]);
+    if (complaintText.trim() === '' || billId.trim() === '' || attachedDocuments.length === 0) {
+      // You can display an error message here or prevent the submission if fields are empty
+      return;
     }
+
+    const newComplaint: Complaint = {
+      id: complaints.length + 1,
+      text: complaintText,
+      documents: attachedDocuments,
+      billId: billId,
+    };
+    setComplaints([...complaints, newComplaint]);
+    setComplaintText('');
+    setBillId('');
+    setAttachedDocuments([]);
   };
 
   const handleDocumentUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -49,7 +57,16 @@ export default function ComplaintsPage() {
   return (
     <Container maxW="xl" mt={10}>
       <Heading mb={4}>Complaints</Heading>
-      <FormControl>
+      <FormControl isRequired>
+        <FormLabel>Bill ID</FormLabel>
+        <Input
+          value={billId}
+          onChange={(e) => setBillId(e.target.value)}
+          placeholder="Enter bill ID"
+          mb={2}
+        />
+      </FormControl>
+      <FormControl isRequired>
         <FormLabel>Add Complaint</FormLabel>
         <Input
           value={complaintText}
@@ -57,15 +74,19 @@ export default function ComplaintsPage() {
           placeholder="Enter complaint text"
           mb={2}
         />
-        <input type="file" multiple onChange={handleDocumentUpload} />
-        <Button onClick={handleComplaintSubmit} colorScheme="blue">
-          Submit
-        </Button>
       </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Attach Documents</FormLabel>
+        <input type="file" multiple onChange={handleDocumentUpload} />
+      </FormControl>
+      <Button onClick={handleComplaintSubmit} colorScheme="blue">
+        Submit
+      </Button>
       <VStack mt={4} align="start" spacing={4}>
         {complaints.map((complaint) => (
           <Box key={complaint.id} p={4} border="1px" borderColor="gray.200" borderRadius="md">
             <Text>{complaint.text}</Text>
+            <Text>Bill ID: {complaint.billId}</Text>
             {complaint.documents.length > 0 && (
               <VStack align="start" mt={2} spacing={1}>
                 <Text fontWeight="bold">Attached Documents:</Text>
